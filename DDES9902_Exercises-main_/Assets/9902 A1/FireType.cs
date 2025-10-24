d4ebae819e3f4cbed165d595e59ef3ffd1cdec2c
YUKUN-YANG-Ryan <yangyukun995@gmail.com>
2025-10-25 00:06:04 +1100

    A1

---
+++ DDES9902_Exercises-main_/Assets/9902 A1/FireType.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+
+public enum ExtinguisherType
+{
+    Water,      // paper,wood
+    Foam,       // oil
+    CO2,        // electric device
+    DryPowder,  // oil,liquid
+}
---
+++ DDES9902_Exercises-main_/Assets/9902 A1/MyPlayer.cs
@@ -0,0 +1,29 @@
+using _9902_A1;
+using UnityEngine;
+
+public class MyPlayer : MonoBehaviour
+{
+    RaycastInteractor rayInteractor;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rayInteractor = GetComponent<RaycastInteractor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (rayInteractor.moveSubject != null)
+            {
+                var extinguisher = rayInteractor.moveSubject.GetComponent<Extinguisher>();
+                if (extinguisher != null)
+                {
+                    extinguisher.Spray();
+                }
+            }
+        }
+    }
+}
